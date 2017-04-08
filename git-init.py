0414802812b7bbdac17321d67b87181e2df81490
jzhang046 <jzhang046@e.ntu.edu.sg>
2017-04-08 14:12:25 -0400

    Fixed font and file size indication and
    Set bold font for speed and completed percentage indications. 
    Fixed file size indication, so that it will not exceed 3 places for the integer part. 
    Fixed the length of progress bar. The total available number of places in one line should be 37.

--- git-init.py
+++ git-init.py
@@ -13,7 +13,7 @@ import math
 def printProgressBar(progress):
     #This function is used to print a progress bar. 
     #This bar is static. 
-    l = 40
+    l = 37
     for i in range(0,int(round(l*progress))):
         sys.stdout.write('\033[100m \033[0m')
         #Dark grey. This will look darker than black ([40m). 
@@ -27,23 +27,23 @@ def printProgressBar(progress):
 
 
 def convSign(length):
-    #If want to limit the int part to occupy less than 4 places, 
-    #use '< math.pow(1000,1)' instead of 1024
+    #To limit the int part to occupy less than 4 places, 
+    #use this instead of '< math.pow(1024, *)'
 
-    if length < math.pow(1024, 1):
-        #less than 1KB
+    if length < 1000:
+        #less than 1000B
         rtn = '%.2fB' % float(length)
-    elif length < math.pow(1024, 2):
-        #less han 1MB
+    elif length < math.pow(1024, 1) * 1000:
+        #less han 1000KB
         rtn = '%.2fKB' % (float(length) / math.pow(1024, 1))
-    elif length < math.pow(1024, 3):
-        #less than 1GB
+    elif length < math.pow(1024, 2) * 1000:
+        #less than 1000MB
         rtn = '%.2fMB' % (float(length) / math.pow(1024, 2))
-    elif length < math.pow(1024, 4):
-        #less than 1TB
+    elif length < math.pow(1024, 3) * 1000:
+        #less than 1000GB
         rtn = '%.2fGB' % (float(length) / math.pow(1024, 3))
     else:
-        rtn = 'Too large' 
+        rtn = 'TooLarge' 
     
     return rtn
 
@@ -72,13 +72,15 @@ def printItems(type, source, token):
         #Get donwload status, e.g. paused, complete. 
 
         print(name[-36:])#Limit the filelength. 
-        sys.stdout.write('Speed: %.2fKB/s  ' % (float(speed)/1024))
+        sys.stdout.write('Speed: \033[1m%.2fKB/s\033[0m  ' % (float(speed)/1024))
         sys.stdout.write('%s\n' % status)
+        #Blod font for speed. 
 
         #Error handling for file length. 
         #To avoid devided by 0. 
         if ttL != 0:
-            sys.stdout.write('Completed: %.2f%%' % (float(cpltL)/ttL*100))
+            sys.stdout.write('Completed: \033[1m%.2f%%\033[0m' % (float(cpltL)/ttL*100))
+            #Bold font for completed percentage. 
             sys.stdout.write('  %(cpltlC)s/%(ttlC)s\n' % {'ttlC':convSign(ttL), 'cpltlC':convSign(cpltL)});
             printProgressBar(float(cpltL)/ttL)
         else:
